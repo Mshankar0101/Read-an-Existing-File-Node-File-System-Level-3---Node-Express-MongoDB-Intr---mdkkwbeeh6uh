151a0498d77a4ce0e78a5abec89505a2f151e453
mshankar0101 <madhushankar0101@gmail.com>
2024-02-20 03:13:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ const fileName = "myfile.txt";
 const reader = async (fileName) => {
   // write code here
 	// dont change function name
+  // const content =  await fs.readFile(fileName);
+  // console.log(content.toString());
+  // return  content.toString();
+
+
+  await fs.readFile(fileName, (err,content)=>{
+    if(err){
+      console.log(err.message);
+    }
+    else{
+       return content.toString();
+    }
+  })
   
 };
 
